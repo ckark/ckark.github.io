efec1dd1bd502e044c8ffe74ae2e636b000252c3
Chris Kark <ckark@alumni.stanford.edu>
2020-03-11 08:11:53 -0600

    code optimizations

--- main.js
+++ main.js
@@ -1,66 +1,33 @@
-Array.from(document.getElementsByClassName('rate')).forEach(e => e.addEventListener('input', function () {
-    this.value = this.value.replace(/[^\d/\.]/, '')
-        .replace(/\.\./g, '')
-        .replace(/\s\s/g, '')
-        .replace(/\s\.\s/g, '');
-    if (this.value !== '') this.value = this.value;
-    if (this.value === '00' || this.value === '000' || this.value === '0.000') this.value = '0';
-    if (Number(this.value) > 12) this.value = '12';
-}))
-
-const getSubtotal = (a, b, c, d, e) => {
-    a = document.getElementById(a);
-    b = document.getElementById(b);
-    c = document.getElementById(c);
-    c.textContent = '$0.00';
-    a.addEventListener('input', function () {
-        c.textContent = ((a.value * d) + (b.value * e)).toLocaleString('en-US', {
-            style: 'currency',
-            currency: 'USD'
-        });
-        if (c.textContent === "$NaN") c.textContent = '$0.00';
+Array.from(document.getElementsByClassName("rate")).forEach(t => t.addEventListener("input", function () {
+    this.value = this.value.replace(/[^\d\/\.]/, "").replace(/\.\./g, "").replace(/\s\s/g, "").replace(/\s\.\s/g, ""), "" !== this.value && (this.value = this.value), "00" !== this.value && "000" !== this.value && "0.000" !== this.value || (this.value = "0"), Number(this.value) > 12 && (this.value = "12")
+}));
+const getSubtotal = (t, e, n, u, l) => {
+    t = document.getElementById(t), e = document.getElementById(e), (n = document.getElementById(n)).textContent = "$0.00", t.addEventListener("input", function () {
+        n.textContent = (t.value * u + e.value * l).toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD"
+        }), "$NaN" === n.textContent && (n.textContent = "$0.00")
+    }), e.addEventListener("input", function () {
+        n.textContent = (t.value * u + e.value * l).toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD"
+        }), "$NaN" === n.textContent && (n.textContent = "$0.00")
     })
-    b.addEventListener('input', function () {
-        c.textContent = ((a.value * d) + (b.value * e)).toLocaleString('en-US', {
-            style: 'currency',
-            currency: 'USD'
-        });
-        if (c.textContent === "$NaN") c.textContent = '$0.00';
-    });
 };
-
-getSubtotal('m12', 'm15', 'smsub', 12, 15);
-getSubtotal('t12', 't15', 'tsub', 12, 15);
-getSubtotal('w12', 'w15', 'wsub', 12, 15);
-getSubtotal('th12', 'th15', 'thsub', 12, 15);
-getSubtotal('f12', 'f15', 'fsub', 12, 15);
-
-document.getElementById('total').textContent = "$0.00";
-
-document.addEventListener('change', e => {
-    const r = (a, b) => a + b;
-    let arr = [Number(document.getElementById('smsub').textContent.replace(/[$,\s]/g, '')), Number(document.getElementById('tsub').textContent.replace(/[$,\s]/g, '')), Number(document.getElementById('wsub').textContent.replace(/[$,\s]/g, '')), Number(document.getElementById('thsub').textContent.replace(/[$,\s]/g, '')), Number(document.getElementById('fsub').textContent.replace(/[$,\s]/g, ''))];
-    document.getElementById('total').textContent = arr.reduce(r).toLocaleString('en-US', {
-        style: 'currency',
-        currency: 'USD'
-    });
-    if (document.getElementById('total').textContent === "$NaN") document.getElementById('total').textContent = '$0.00';
-})
-
-// Shake Erroneous Field Values on Blur
-const validate = e => {
-    Array.from(document.getElementsByClassName(e)).forEach(el => {
-        el.addEventListener('blur', function () {
-            if (el.checkValidity() === false || el.value === '') {
-                el.classList.add('error');
-                setTimeout(function () {
-                    el.classList.remove('error');
-                }, 300);
-                event.preventDefault();
-            } else {
-                el.style.backgroundColor = null;
-            }
-        });
-    });
+getSubtotal("m12", "m15", "smsub", 12, 15), getSubtotal("t12", "t15", "tsub", 12, 15), getSubtotal("w12", "w15", "wsub", 12, 15), getSubtotal("th12", "th15", "thsub", 12, 15), getSubtotal("f12", "f15", "fsub", 12, 15), document.getElementById("total").textContent = "$0.00", document.addEventListener("change", t => {
+    let e = [Number(document.getElementById("smsub").textContent.replace(/[$,\s]/g, "")), Number(document.getElementById("tsub").textContent.replace(/[$,\s]/g, "")), Number(document.getElementById("wsub").textContent.replace(/[$,\s]/g, "")), Number(document.getElementById("thsub").textContent.replace(/[$,\s]/g, "")), Number(document.getElementById("fsub").textContent.replace(/[$,\s]/g, ""))];
+    document.getElementById("total").textContent = e.reduce((t, e) => t + e).toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD"
+    }), "$NaN" === document.getElementById("total").textContent && (document.getElementById("total").textContent = "$0.00")
+});
+const validate = t => {
+    Array.from(document.getElementsByClassName(t)).forEach(t => {
+        t.addEventListener("blur", function () {
+            !1 === t.checkValidity() || "" === t.value ? (t.classList.add("error"), setTimeout(function () {
+                t.classList.remove("error")
+            }, 300), event.preventDefault()) : t.style.backgroundColor = null
+        })
+    })
 };
-validate('rate')
+validate("rate");
